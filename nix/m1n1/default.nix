64476ba8570f5f0bcadb4c0123b8fe8a8ca804ba
Thomas Watson <twatson52@icloud.com>
2021-12-13 21:55:49 -0600

    fix m1n1 toolchain paths

--- nix/m1n1/default.nix
+++ nix/m1n1/default.nix
@@ -9,7 +9,7 @@
   pyenv = python3.withPackages (p: with p; [
     construct
     pyserial
-  ]); 
+  ]);
 in stdenv.mkDerivation {
   pname = "m1n1";
   version = "unstable-2021-12-13";
@@ -30,10 +30,16 @@ in stdenv.mkDerivation {
     pkgsCross.aarch64-multiplatform.buildPackages.gcc
   ];
 
+  postPatch = ''
+    substituteInPlace proxyclient/m1n1/asm.py \
+      --replace 'aarch64-linux-gnu-' 'aarch64-unknown-linux-gnu-' \
+      --replace 'TOOLCHAIN = ""' 'TOOLCHAIN = "'$out'/toolchain-bin/"'
+  '';
+
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/{bin,build,script}
+    mkdir -p $out/{bin,build,script,toolchain-bin}
     cp -r proxyclient $out/script
     cp -r tools $out/script
     cp build/m1n1.macho $out/build
@@ -47,6 +53,16 @@ ${pyenv}/bin/python $toolpath "\$@"
 EOF
       chmod +x $script
     done
+
+    GCC=${pkgsCross.aarch64-multiplatform.buildPackages.gcc}
+    BINUTILS=${pkgsCross.aarch64-multiplatform.buildPackages.binutils}
+    REAL_BINUTILS=$(grep -o '/nix/store/[^ ]*binutils[^ ]*' $BINUTILS/nix-support/propagated-user-env-packages)
+
+    ln -s $GCC/bin/*-gcc $out/toolchain-bin/
+    ln -s $GCC/bin/*-ld $out/toolchain-bin/
+    ln -s $REAL_BINUTILS/bin/*-objcopy $out/toolchain-bin/
+    ln -s $REAL_BINUTILS/bin/*-objdump $out/toolchain-bin/
+    ln -s $REAL_BINUTILS/bin/*-nm $out/toolchain-bin/
 
     runHook postInstall
   '';
