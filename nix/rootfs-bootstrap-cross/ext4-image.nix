568400bf24eade01ea950b9c2d23b3ea20f4de23
Thomas Watson <twatson52@icloud.com>
2021-12-19 17:47:08 -0600

    avoid trying to resize the root partition itself, just in case

--- nix/rootfs-bootstrap-cross/ext4-image.nix
+++ nix/rootfs-bootstrap-cross/ext4-image.nix
@@ -54,5 +54,28 @@ in
         ln -s ${compressedImage} $img
       '';
     }) {};
+
+    sdImage.expandOnBoot = lib.mkForce false;
+
+    boot.postBootCommands = ''
+      # On the first boot do some maintenance tasks
+      if [ -f /nix-path-registration ]; then
+        set -euo pipefail
+        set -x
+        # Figure out device names for the boot device and root filesystem.
+        rootPart=$(${pkgs.util-linux}/bin/findmnt -n -o SOURCE /)
+        bootDevice=$(lsblk -npo PKNAME $rootPart)
+        partNum=$(lsblk -npo MAJ:MIN $rootPart | ${pkgs.gawk}/bin/awk -F: '{print $2}')
+        # Resize the filesystem to fit the disk
+        ${pkgs.e2fsprogs}/bin/resize2fs $rootPart
+        # Register the contents of the initial Nix store
+        ${config.nix.package.out}/bin/nix-store --load-db < /nix-path-registration
+        # nixos-rebuild also requires a "system" profile and an /etc/NIXOS tag.
+        touch /etc/NIXOS
+        ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
+        # Prevents this from running on later boots.
+        rm -f /nix-path-registration
+      fi
+    '';
   };
 }
