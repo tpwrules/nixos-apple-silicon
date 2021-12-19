08515ae4324cc11da5af25b4ba247d15a54bb986
Thomas Watson <twatson52@icloud.com>
2021-12-19 14:53:40 -0600

    make it possible to uncompress rootfs on the mac

--- nix/rootfs-bootstrap-cross/ext4-image.nix
+++ nix/rootfs-bootstrap-cross/ext4-image.nix
@@ -7,12 +7,30 @@ with lib;
 let
   rootfsImage = pkgs.callPackage "${pkgs.path}/nixos/lib/make-ext4-fs.nix" ({
     inherit (config.sdImage) storePaths;
-    compressImage = config.sdImage.compressImage;
+    compressImage = false;
     populateImageCommands = config.sdImage.populateRootCommands;
     volumeLabel = "NIXOS_SD";
   } // optionalAttrs (config.sdImage.rootPartitionUUID != null) {
     uuid = config.sdImage.rootPartitionUUID;
   });
+
+  # we use tar and gzip to compress the image instead of the default zstd so
+  # that it can be transferred and decompressed in recoveryOS (which doesn't
+  # even have bare gzip, only tar!!!)
+  compressedImage = if !config.sdImage.compressImage then rootfsImage else (
+    pkgs.callPackage ({ pigz }: rootfsImage.overrideAttrs (o: {
+      name = o.name + ".tar.gz";
+      nativeBuildInputs = o.nativeBuildInputs ++ [ pigz ];
+      buildCommand = ''
+        imgout=$out
+        out=nixos_root.img
+      '' + o.buildCommand + ''
+        out=$imgout
+        echo "Compressing image"
+        tar c nixos_root.img | pigz -p $NIX_BUILD_CORES -c > $out
+      '';
+    })
+  )) {};
 in
 {
   config = {
@@ -25,15 +43,15 @@ in
 
       buildCommand = ''
         mkdir -p $out/nix-support $out/ext4-image
-        export img=$out/ext4-image/${ext4Name}
+        export img=$out/ext4-image/nixos_root.img
         if test -n "$compressImage"; then
-            export img=$img.zst
+            export img=$img.tar.gz
         fi
 
         echo "${pkgs.stdenv.buildPlatform.system}" > $out/nix-support/system
         echo "file ext4-image $img" >> $out/nix-support/hydra-build-products
 
-        ln -s ${rootfsImage} $img
+        ln -s ${compressedImage} $img
       '';
     }) {};
   };
