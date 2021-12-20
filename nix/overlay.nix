d21c62fe457d2dac2eca12e58441960cc74435dc
Thomas Watson <twatson52@icloud.com>
2021-12-19 18:49:40 -0600

    add ability to generate u-boots for different devices

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -2,11 +2,25 @@ self: super:
 let
   pkgs = self;
   lib = pkgs.lib;
+
+  # u-boot only works on M1 regular for now.
+  # device name to DTB name list is available here:
+  # https://github.com/AsahiLinux/docs/wiki/Devices
+  # e.g. the M1 2020 Mac Mini's device tree name is t8103-j274
+  compatibleDTs = [
+    "t8103-j274"
+    "t8103-j293"
+    "t8103-j313"
+    "t8103-j456"
+    "t8103-j457"
+  ];
 in {
   # main scope
   nixos-m1 = lib.makeScope pkgs.newScope (self: with self; {
     m1n1 = callPackage ./m1n1 {};
-    u-boot = callPackage ./u-boot {};
+    u-boot = lib.genAttrs compatibleDTs (
+      name: callPackage ./u-boot { withDeviceTree = name; }
+    );
     rootfs-bootstrap-cross = callPackage ./rootfs-bootstrap-cross {};
   });
 }
