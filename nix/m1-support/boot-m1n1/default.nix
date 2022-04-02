118ff7acb45f01c9eed4ca5503bb0b06e731eafb
Thomas Watson <twatson52@icloud.com>
2022-04-02 12:41:53 -0500

    add option to append options to m1n1 boot binary

--- nix/m1-support/boot-m1n1/default.nix
+++ nix/m1-support/boot-m1n1/default.nix
@@ -22,13 +22,30 @@ let
   };
 
   bootFiles = {
-    "m1n1/boot.bin" = "${boot}/m1n1-u-boot.bin";
+    "m1n1/boot.bin" = if config.boot.m1n1ExtraOptions == "" then
+      "${boot}/m1n1-u-boot.bin"
+    else pkgs.runCommand "boot.bin" {} ''
+      cat ${boot}/m1n1-u-boot.bin > $out
+      echo '${config.boot.m1n1ExtraOptions}' >> $out
+    '';
   };
 in {
-  # install m1n1 with the boot loader
-  boot.loader.grub.extraFiles = bootFiles;
-  boot.loader.systemd-boot.extraFiles = bootFiles;
+  config = {
+    # install m1n1 with the boot loader
+    boot.loader.grub.extraFiles = bootFiles;
+    boot.loader.systemd-boot.extraFiles = bootFiles;
 
-  # ensure the installer has m1n1 in the image
-  system.extraDependencies = lib.mkForce [ boot ];
+    # ensure the installer has m1n1 in the image
+    system.extraDependencies = lib.mkForce [ boot ];
+  };
+
+  options.boot.m1n1ExtraOptions = lib.mkOption {
+    type = lib.types.str;
+    default = "";
+    description = ''
+      Append extra options to the m1n1 boot binary. Might be useful for fixing
+      display problems on Mac minis.
+      https://github.com/AsahiLinux/m1n1/issues/159
+    '';
+  };
 }
