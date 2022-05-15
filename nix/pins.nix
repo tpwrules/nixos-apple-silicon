e09a233ed5d242d356980cd3109c21dc9975e377
Thomas Watson <twatson52@icloud.com>
2022-05-15 11:47:02 -0500

    update nixpkgs

--- nix/pins.nix
+++ nix/pins.nix
@@ -2,9 +2,9 @@
   # https://hydra.nixos.org/jobset/mobile-nixos/unstable/evals
   # these evals have a cross-compiled stdenv available
   nixpkgs = fetchTarball {
-    name   = "nixpkgs-unstable-2022-05-10";
-    url    = "https://github.com/NixOS/nixpkgs/archive/2a3aac479caeba0a65b2ad755fe5f284f1fde74d.tar.gz";
-    sha256 = "sha256:0px2fk64s56qxd8ir8xg8bsj5yz1w399ps4xfkyx29n2ywp9ar7c";
+    name   = "nixpkgs-unstable-2022-05-14";
+    url    = "https://github.com/NixOS/nixpkgs/archive/d89d7af1ba23bd8a5341d00bdd862e8e9a808f56.tar.gz";
+    sha256 = "sha256:09j336xnvwbq86v27xcbpmfjwjfzng7d0sdcrbpv60hc42dpdr8d";
   };
 
   rust-overlay = fetchTarball {
