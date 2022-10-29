2682a24734e1599bc1bf960b07ecbd497dab438c
Thomas Watson <twatson52@icloud.com>
2022-10-29 16:31:22 -0500

    upgrade firmware extractor

--- nix/m1-support/asahi-fwextract/default.nix
+++ nix/m1-support/asahi-fwextract/default.nix
@@ -1,24 +1,36 @@
 { lib
 , python3
 , fetchFromGitHub
-, makeBinaryWrapper
+, gzip
+, gnutar
+, lzfse
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "asahi-fwextract";
-  version = "0.4pre2";
+  version = "0.5pre2";
 
   # tracking version: https://github.com/AsahiLinux/PKGBUILDs/blob/main/asahi-fwextract/PKGBUILD
   src = fetchFromGitHub {
     owner = "AsahiLinux";
     repo = "asahi-installer";
     rev = "v${version}";
-    hash = "sha256-RqvD2hNjKMlUg+oY1woUN5zpN+1Y/TrBQbokNgdeCW4=";
+    hash = "sha256-p34eN2iE1s8rupdysjyf6GN8kHkVG9NDw31YKPDNXbk=";
   };
 
   patches = [
     ./add_entry_point.patch
   ];
 
-  nativeBuildInputs = [ python3.pkgs.setuptools makeBinaryWrapper ];
+  postPatch = ''
+    substituteInPlace asahi_firmware/img4.py \
+      --replace 'liblzfse.so' '${lzfse}/lib/liblzfse.so'
+    substituteInPlace asahi_firmware/update.py \
+      --replace '"tar"' '"${gnutar}/bin/tar"' \
+      --replace '"xf"' '"-x", "-I", "${gzip}/bin/gzip", "-f"'
+  '';
+
+  nativeBuildInputs = [ python3.pkgs.setuptools ];
+
+  doCheck = false;
 }
