a085252cc9be66dca2f623c8f04be99e41f61f43
Natsu Kagami <nki@nkagami.me>
2022-11-23 13:39:08 +0100

    Build 6.1 edge kernel

--- nix/m1-support/kernel/package.nix
+++ nix/m1-support/kernel/package.nix
@@ -1,11 +1,12 @@
-{ pkgs, _4KBuild ? false }: let
+{ pkgs, _4KBuild ? false }:
+let
   localPkgs =
     # we do this so the config can be read on any system and not affect
     # the output hash
     if builtins ? currentSystem then import (pkgs.path) { system = builtins.currentSystem; }
     else pkgs;
 
-  readConfig = configfile: import (localPkgs.runCommand "config.nix" {} ''
+  readConfig = configfile: import (localPkgs.runCommand "config.nix" { } ''
     echo "{" > "$out"
     while IFS='=' read key val; do
       [ "x''${key#CONFIG_}" != "x$key" ] || continue
@@ -16,42 +17,47 @@
   '').outPath;
 
   linux_asahi_pkg = { stdenv, lib, fetchFromGitHub, fetchpatch, linuxKernel, ... } @ args:
-    linuxKernel.manualConfig rec {
-      inherit stdenv lib;
+    linuxKernel.manualConfig
+      rec {
+        inherit stdenv lib;
 
-      version = "5.19.0-asahi";
-      modDirVersion = version;
+        version = "6.1.0-rc6-asahi";
+        modDirVersion = version;
 
-      src = fetchFromGitHub {
-        # tracking branch: https://github.com/AsahiLinux/linux/tree/asahi
-        owner = "AsahiLinux";
-        repo = "linux";
-        rev = "f8c0d18173a7b649999ee27515393f7aae40310c";
-        hash = "sha256-6Pceu4eBF8kl//8CV57+yfYnywZxNk17BU0YgLkGVc0=";
-      };
+        src = fetchFromGitHub {
+          # tracking branch: https://github.com/AsahiLinux/linux/tree/asahi
+          owner = "AsahiLinux";
+          repo = "linux";
+          rev = "asahi-6.1-rc6-5";
+          hash = "sha256-HHPfAtNh5wR0TCsEYuMdSbp55p1IVhF07tg4dlfgXk0=";
+        };
 
-      kernelPatches = [
-      ] ++ lib.optionals _4KBuild [
-        # thanks to Sven Peter
-        # https://lore.kernel.org/linux-iommu/20211019163737.46269-1-sven@svenpeter.dev/
-        { name = "sven-iommu-4k";
-          patch = ./sven-iommu-4k.patch;
-        }
-      ] ++ lib.optionals (!_4KBuild) [
-        # patch the kernel to set the default size to 16k instead of modifying
-        # the config so we don't need to convert our config to the nixos
-        # infrastructure or patch it and thus introduce a dependency on the host
-        # system architecture
-        { name = "default-pagesize-16k";
-          patch = ./default-pagesize-16k.patch;
-        }
-      ];
+        kernelPatches = [
+        ] ++ lib.optionals _4KBuild [
+          # thanks to Sven Peter
+          # https://lore.kernel.org/linux-iommu/20211019163737.46269-1-sven@svenpeter.dev/
+          {
+            name = "sven-iommu-4k";
+            patch = ./sven-iommu-4k.patch;
+          }
+        ] ++ lib.optionals (!_4KBuild) [
+          # patch the kernel to set the default size to 16k instead of modifying
+          # the config so we don't need to convert our config to the nixos
+          # infrastructure or patch it and thus introduce a dependency on the host
+          # system architecture
+          {
+            name = "default-pagesize-16k";
+            patch = ./default-pagesize-16k.patch;
+          }
+        ];
 
-      configfile = ./config;
-      config = readConfig configfile;
+        configfile = ./config;
+        config = readConfig configfile;
 
-      extraMeta.branch = "5.19";
-    } // (args.argsOverride or {});
+        extraMeta.branch = "6.1";
+      } // (args.argsOverride or { });
 
   linux_asahi = (pkgs.callPackage linux_asahi_pkg { });
-in pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_asahi)
+in
+pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_asahi)
+
