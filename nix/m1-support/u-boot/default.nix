000e63584169047ecca1f62a4f6c78d9c2f126e8
Zzy Wysm <zzy@zzywysm.com>
2022-12-23 16:33:58 -0500

    u-boot: add in and enable bigger fonts
    the default u-boot font is so so tiny.  this fixes that.
    
    Signed-off-by: Zzy Wysm <zzy@zzywysm.com>

--- nix/m1-support/u-boot/default.nix
+++ nix/m1-support/u-boot/default.nix
@@ -1,5 +1,6 @@
 { lib
 , fetchFromGitHub
+, fetchpatch
 , pkgs
 , pkgsCross
 , m1n1
@@ -25,10 +26,28 @@ in (buildPkgs.buildUBoot rec {
   ];
   extraConfig = ''
     CONFIG_IDENT_STRING=" ${version}"
+    CONFIG_VIDEO_FONT_4X6=n
+    CONFIG_VIDEO_FONT_8X16=n
+    CONFIG_VIDEO_FONT_SUN12X22=n
+    CONFIG_VIDEO_FONT_TER12X24=n
+    CONFIG_VIDEO_FONT_TER16X32=y
   '';
 }).overrideAttrs (o: {
   # nixos's downstream patches are not applicable
-  patches = [ ];
+  # however, we add in bigger u-boot fonts because the mac laptop screens are high-res
+  patches = [ 
+    (fetchpatch {
+      url = "https://git.alpinelinux.org/aports/plain/testing/u-boot-asahi/apritzel-first5-video.patch?id=990110f35b50b74bdb4e902d94fa15b07a8eac9e";
+      sha256 = "sha256-QPvJYxIcQBHbwsj7l96qGUZSipk1sB3ZyniD1Io18dY=";
+      revert = false;
+    })
+
+    (fetchpatch {
+      url = "https://git.alpinelinux.org/aports/plain/testing/u-boot-asahi/mps-u-boot-ter12x24.patch?id=990110f35b50b74bdb4e902d94fa15b07a8eac9e";
+      sha256 = "sha256-wrQpIYiuNRi/p2p290KCGPmuRxFEOPlbICoFvd+E8p0=";
+      revert = false;
+    })
+  ];
 
   preInstall = ''
     # compress so that m1n1 knows U-Boot's size and can find things after it
