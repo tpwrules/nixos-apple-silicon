cbd3029a9c4d98616677a787aeefa70eb363b402
Erin <79354991+oati@users.noreply.github.com>
2023-01-15 12:14:57 -0600

    remove default.nix and pins.nix

--- default.nix
+++
@@ -1,9 +0,0 @@
-let
-  pins = import ./nix/pins.nix;
-  overlays = [
-    (import ./nix/overlay.nix)
-    (import pins.rust-overlay)
-  ];
-in {
-  nixpkgs ? pins.nixpkgs
-}: (import nixpkgs { overlays = overlays; }).nixos-m1
