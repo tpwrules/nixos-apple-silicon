90d27c4d46c1e0a8e014ed70bdac46397761e383
Thomas Watson <twatson52@icloud.com>
2023-01-16 12:25:13 -0600

    replace system Mesa when using experimental GPU support
    This avoids breaking programs like Plasma Wayland due to either the slight
    Mesa version mismatch or the adjustments made for Asahi GPU support.
    
    `system.replaceRuntimeDependencies` is used instead of an overlay to avoid
    rebuilding the world for this replacement. It's possible this might cause
    its own issues but this is all experimental anyway.

--- nix/m1-support/mesa/default.nix
+++ nix/m1-support/mesa/default.nix
@@ -3,6 +3,19 @@
   config = let
     mesaAsahi = pkgs.callPackage ./package.nix { };
   in lib.mkIf config.hardware.asahi.useExperimentalGPUDriver {
+    # some programs (like Plasma Wayland) are broken if the version of
+    # Mesa they link against is different to the one driving the
+    # graphics card. replace the Mesa linked into system packages with
+    # the Asahi one without rebuilding the world. it's unclear if the
+    # issue is simply slightly different Mesa versions or the
+    # modifications required for the Apple GPU but the replacement is
+    # safe enough and this is all experimental anyway.
+    system.replaceRuntimeDependencies = [
+      { original = pkgs.mesa;
+        replacement = mesaAsahi;
+      }
+    ];
+
     hardware.opengl.package = mesaAsahi.drivers;
 
     # required for GPU kernel driver
