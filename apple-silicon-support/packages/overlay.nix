27ff0705a6b26631e10dc8dfb73e47e4bf2342d7
Erin <79354991+oati@users.noreply.github.com>
2023-01-30 10:23:19 -0600

    fix mesa overlay

--- apple-silicon-support/packages/overlay.nix
+++ apple-silicon-support/packages/overlay.nix
@@ -3,6 +3,6 @@ final: prev: {
   m1n1 = final.callPackage ./m1n1 { };
   uboot-asahi = final.callPackage ./uboot-asahi { };
   asahi-fwextract = final.callPackage ./asahi-fwextract { };
-  mesa-asahi-edge = final.callPackage ./mesa-asahi-edge { };
+  mesa-asahi-edge = final.callPackage ./mesa-asahi-edge { inherit (prev) mesa; };
   # TODO: package alsa-ucm-conf-asahi for headphone jack support
 }
