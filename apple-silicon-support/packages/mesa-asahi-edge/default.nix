d912bf1a0c87d5dd56e8540dfdc2dbb73c4bf941
Thomas Watson <twatson52@icloud.com>
2023-03-21 20:19:58 -0500

    fix build of mesa-asahi-edge

--- apple-silicon-support/packages/mesa-asahi-edge/default.nix
+++ apple-silicon-support/packages/mesa-asahi-edge/default.nix
@@ -18,7 +18,24 @@
     rev = "asahi-20230311";
     hash = "sha256-Qy1OpjTohSDGwONK365QFH9P8npErswqf2TchUxR1tQ=";
   };
-  # remove flag to configure xvmc functionality as having it
-  # breaks the build because that no longer exists in Mesa 23
-  mesonFlags = lib.filter (x: !(lib.hasPrefix "-Dxvmc-libs-path=" x)) oldAttrs.mesonFlags;
+
+  mesonFlags =
+    # remove flag to configure xvmc functionality as having it
+    # breaks the build because that no longer exists in Mesa 23
+    (lib.filter (x: !(lib.hasPrefix "-Dxvmc-libs-path=" x)) oldAttrs.mesonFlags) ++ [
+      # we do not build any graphics drivers these features can be enabled for
+      "-Dgallium-va=disabled"
+      "-Dgallium-vdpau=disabled"
+      "-Dgallium-xa=disabled"
+      # does not make any sense
+      "-Dandroid-libbacktrace=disabled"
+      # do not want to add the dependencies
+      "-Dlibunwind=disabled"
+      "-Dlmsensors=disabled"
+    ];
+
+  # replace disk cache path patch with one tweaked slightly to apply to this version
+  patches = lib.forEach oldAttrs.patches
+    (p: if lib.hasSuffix "disk_cache-include-dri-driver-path-in-cache-key.patch" p
+      then ./disk_cache-include-dri-driver-path-in-cache-key.patch else p);
 })
