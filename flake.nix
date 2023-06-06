9ae6422f8ea4f198ed6db4af2beda4d4e46adde3
Natsu Kagami <nki@nkagami.me>
2023-06-06 20:30:23 +0200

    Update linux-asahi to 6.3-6

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
               ];
             };
           in {
-            inherit (pkgs) m1n1 uboot-asahi;
+            inherit (pkgs) m1n1 uboot-asahi linux-asahi;
 
             installer-bootstrap =
               let
