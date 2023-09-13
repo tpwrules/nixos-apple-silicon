78d4fbc8d84fc85f5d5b819bc5995baa8ae431da
Ion Agorria <ion@agorria.com>
2023-09-13 17:07:45 +0200

    alsa-ucm-conf-asahi: Add support for audio confs

---
+++ apple-silicon-support/packages/alsa-ucm-conf-asahi/default.nix
@@ -0,0 +1,20 @@
+{ lib
+, fetchFromGitHub
+, alsa-ucm-conf
+}:
+
+(alsa-ucm-conf.overrideAttrs (oldAttrs: rec {
+  version = "3";
+
+  src_asahi = fetchFromGitHub {
+    # tracking: https://github.com/AsahiLinux/PKGBUILDs/blob/main/alsa-ucm-conf-asahi/PKGBUILD
+    owner = "AsahiLinux";
+    repo = "alsa-ucm-conf-asahi";
+    rev = "v${version}";
+    hash = "sha256-TCCT0AJx0SdnTzzBaV94zuD2hrPqvk+9vTTuEQmpJjc=";
+  };
+
+  postInstall = oldAttrs.postInstall or "" + ''
+    cp -r ${src_asahi}/ucm2 $out/share/alsa
+  '';
+}))
