7041e945d76df7af322092ad1d7996ebe451d939
Yureka <yuka@yuka.dev>
2023-11-29 01:27:12 +0100

    alsa-ucm-conf: 3 -> 4

--- apple-silicon-support/packages/alsa-ucm-conf-asahi/default.nix
+++ apple-silicon-support/packages/alsa-ucm-conf-asahi/default.nix
@@ -3,14 +3,14 @@
 , alsa-ucm-conf }:
 
 (alsa-ucm-conf.overrideAttrs (oldAttrs: rec {
-  version = "3";
+  version = "4";
 
   src_asahi = fetchFromGitHub {
     # tracking: https://github.com/AsahiLinux/PKGBUILDs/blob/main/alsa-ucm-conf-asahi/PKGBUILD
     owner = "AsahiLinux";
     repo = "alsa-ucm-conf-asahi";
     rev = "v${version}";
-    hash = "sha256-TCCT0AJx0SdnTzzBaV94zuD2hrPqvk+9vTTuEQmpJjc=";
+    hash = "sha256-ZLPpFwWL47Vwc8tRvLo2m/3ocSVNSBHBIRLS+TENr78=";
   };
   
   postInstall = oldAttrs.postInstall or "" + ''
