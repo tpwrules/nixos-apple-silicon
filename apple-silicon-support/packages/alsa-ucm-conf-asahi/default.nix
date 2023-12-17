5002211aed2b383d761473028f6920794cd7dd48
Thomas Watson <twatson52@icloud.com>
2023-12-16 21:18:59 -0600

    alsa-ucm-conf-asahi: v3 -> v5

--- apple-silicon-support/packages/alsa-ucm-conf-asahi/default.nix
+++ apple-silicon-support/packages/alsa-ucm-conf-asahi/default.nix
@@ -3,14 +3,14 @@
 , alsa-ucm-conf }:
 
 (alsa-ucm-conf.overrideAttrs (oldAttrs: rec {
-  version = "3";
+  version = "5";
 
   src_asahi = fetchFromGitHub {
-    # tracking: https://github.com/AsahiLinux/PKGBUILDs/blob/main/alsa-ucm-conf-asahi/PKGBUILD
+    # tracking: https://src.fedoraproject.org/rpms/alsa-ucm-asahi
     owner = "AsahiLinux";
     repo = "alsa-ucm-conf-asahi";
     rev = "v${version}";
-    hash = "sha256-TCCT0AJx0SdnTzzBaV94zuD2hrPqvk+9vTTuEQmpJjc=";
+    hash = "sha256-daUNz5oUrPfSMO0Tqq/WbtiLHMOtPeQQlI+juGrhTxw=";
   };
   
   postInstall = oldAttrs.postInstall or "" + ''
