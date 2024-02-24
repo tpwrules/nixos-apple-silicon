0b10ecc98433e9aac8fd94511100d2e4e799ac79
Thomas Watson <twatson52@icloud.com>
2024-02-24 12:09:03 -0600

    bankstown-lv2: 1.0.3 -> 1.1.0

--- apple-silicon-support/packages/bankstown-lv2/default.nix
+++ apple-silicon-support/packages/bankstown-lv2/default.nix
@@ -9,16 +9,16 @@
 rustPlatform.buildRustPackage rec {
   pname = "bankstown-lv2";
   # tracking: https://src.fedoraproject.org/rpms/rust-bankstown-lv2
-  version = "1.0.3";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "chadmed";
     repo = "bankstown";
     rev = version;
-    hash = "sha256-dPgQuwwY1FEsH65vYClTtV/c+0cB5uq8QYszeHPdIQA=";
+    hash = "sha256-IThXEY+mvT2MCw0PSWU/182xbUafd6dtm6hNjieLlKg=";
   };
 
-  cargoSha256 = "sha256-HIW4mJ1VQSzOIksmJ2d4FQjTfU2Zk6xva1mYUk6MQCI=";
+  cargoSha256 = "sha256-yRzM4tcYc6mweTpLnnlCeKgP00L2wRgHamtUzK9Kstc=";
 
   installPhase = ''
     export LIBDIR=$out/lib
