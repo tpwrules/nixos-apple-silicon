75b06b99f9e344b1195f23600201575d45497be8
Thomas Watson <twatson52@icloud.com>
2024-02-26 22:40:29 -0600

    modules/sound: refactor to use new pipewire/wireplumber options
    Sadly the old way is incompatible now.

--- apple-silicon-support/modules/sound/default.nix
+++ apple-silicon-support/modules/sound/default.nix
@@ -36,34 +36,30 @@
     # enable pipewire to run real-time and avoid audible glitches
     security.rtkit.enable = true;
     # set up pipewire with the supported capabilities (instead of pulseaudio)
+    # and asahi-audio configs and plugins
     services.pipewire = {
       enable = true;
+      configPackages = [ asahi-audio ];
+      extraLv2Packages = [ lsp-plugins pkgs.bankstown-lv2 ];
 
       alsa.enable = true;
-      wireplumber.enable = true;
       pulse.enable = true;
+      wireplumber = {
+        enable = true;
+        configPackages = [ asahi-audio ];
+        extraLv2Packages = [ lsp-plugins pkgs.bankstown-lv2 ];
+      };
     };
+
+    # set up enivronment so that UCM configs are used as well
+    environment.variables.ALSA_CONFIG_UCM2 = "${pkgs.alsa-ucm-conf-asahi}/share/alsa/ucm2";
+    systemd.user.services.pipewire.environment.ALSA_CONFIG_UCM2 = config.environment.variables.ALSA_CONFIG_UCM2;
+    systemd.user.services.wireplumber.environment.ALSA_CONFIG_UCM2 = config.environment.variables.ALSA_CONFIG_UCM2;
 
     # enable speakersafetyd to protect speakers
     systemd.packages = lib.mkAssert lsp-plugins-is-safe
       "lsp-plugins is unpatched/outdated and speakers cannot be safely enabled"
       [ pkgs.speakersafetyd ];
     services.udev.packages = [ pkgs.speakersafetyd ];
-
-    # set up enivronment so that asahi-audio and UCM configs are used
-    environment.etc = builtins.listToAttrs (builtins.map
-      (f: { name = f; value = { source = "${asahi-audio}/share/${f}"; }; })
-      asahi-audio.providedConfigFiles);
-    environment.variables.ALSA_CONFIG_UCM2 = "${pkgs.alsa-ucm-conf-asahi}/share/alsa/ucm2";
-
-    # set up pipewire and wireplumber to use asahi-audio configs and plugins
-    systemd.user.services.pipewire.environment.ALSA_CONFIG_UCM2 = config.environment.variables.ALSA_CONFIG_UCM2;
-    systemd.user.services.wireplumber.environment.ALSA_CONFIG_UCM2 = config.environment.variables.ALSA_CONFIG_UCM2;
-    systemd.user.services.pipewire.environment.LV2_PATH = let
-      lv2Plugins = [ lsp-plugins pkgs.bankstown-lv2 ];
-    in lib.makeSearchPath "lib/lv2" lv2Plugins;
-    systemd.user.services.wireplumber.environment.LV2_PATH = let
-      lv2Plugins = [ lsp-plugins pkgs.bankstown-lv2 ];
-    in lib.makeSearchPath "lib/lv2" lv2Plugins;
   };
 }
