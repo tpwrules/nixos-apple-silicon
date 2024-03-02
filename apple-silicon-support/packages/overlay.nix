c6fc3fc4bd388d5fb609b3bd2663a1416fd3c5ab
Yureka <yuka@yuka.dev>
2024-03-02 04:35:48 +0100

    remove speakersafetyd

--- apple-silicon-support/packages/overlay.nix
+++ apple-silicon-support/packages/overlay.nix
@@ -5,7 +5,6 @@ final: prev: {
   asahi-fwextract = final.callPackage ./asahi-fwextract { };
   mesa-asahi-edge = final.callPackage ./mesa-asahi-edge { inherit (prev) mesa; };
   alsa-ucm-conf-asahi = final.callPackage ./alsa-ucm-conf-asahi { inherit (prev) alsa-ucm-conf; };
-  speakersafetyd = final.callPackage ./speakersafetyd { };
   bankstown-lv2 = final.callPackage ./bankstown-lv2 { };
   asahi-audio = final.callPackage ./asahi-audio { };
 }
