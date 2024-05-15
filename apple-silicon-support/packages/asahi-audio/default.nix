78d6e850e62bd49b48c93a0695aebc68f01d22d6
Thomas Watson <twatson52@icloud.com>
2024-05-14 22:49:36 -0500

    modules/sound: drop compatibility with older nixpkgs
    Not needed for nixpkgses with wireplumber >= 0.5.2.

--- apple-silicon-support/packages/asahi-audio/default.nix
+++ apple-silicon-support/packages/asahi-audio/default.nix
@@ -6,7 +6,6 @@
 stdenv.mkDerivation rec {
   pname = "asahi-audio";
   # tracking: https://src.fedoraproject.org/rpms/asahi-audio
-  # note: ensure that the providedConfigFiles list below is current!
   version = "2.2";
 
   src = fetchFromGitHub {
@@ -35,15 +34,4 @@ stdenv.mkDerivation rec {
     # no need to link the asahi-audio dir globally
     mv $out/share/asahi-audio $out
   '';
-
-  # list of config files installed in $out/share/ and destined for
-  # /etc/, from the `install -pm0644 conf/` lines in the Makefile. note
-  # that the contents of asahi-audio/ stay in $out/ and the config files
-  # are modified to point to them.
-  passthru.providedConfigFiles = [
-    "wireplumber/wireplumber.conf.d/99-asahi.conf"
-    "wireplumber/scripts/device/asahi-limit-volume.lua"
-    "pipewire/pipewire.conf.d/99-asahi.conf"
-    "pipewire/pipewire-pulse.conf.d/99-asahi.conf"
-  ];
 }
