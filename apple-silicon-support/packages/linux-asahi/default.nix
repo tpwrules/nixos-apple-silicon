21718e48abb753a3f5388c62e4656e40cf7b5710
Thomas Watson <twatson52@icloud.com>
2024-07-14 11:37:31 -0500

    linux-asahi: asahi-6.8.10-4 -> asahi-6.9.9-5
    Update for latest config.
    
    Remove now in-tree speaker enablement patches.

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -86,35 +86,21 @@ let
     (linuxKernel.manualConfig rec {
       inherit stdenv lib;
 
-      version = "6.8.10-asahi";
+      version = "6.9.9-asahi";
       modDirVersion = version;
-      extraMeta.branch = "6.8";
+      extraMeta.branch = "6.9";
 
       src = fetchFromGitHub {
         # tracking: https://github.com/AsahiLinux/linux/tree/asahi-wip (w/ fedora verification)
         owner = "AsahiLinux";
         repo = "linux";
-        rev = "asahi-6.8.10-4";
-        hash = "sha256-/UknEMf4awGcb1Pf2YiCfowPv/oIx0zAGNJm0wp1qgA=";
+        rev = "asahi-6.9.9-5";
+        hash = "sha256-l3VH38tqzCHLRYOj0jtKsIsBnqwXqKsA4g+4lEC11lQ=";
       };
 
       kernelPatches = [
         { name = "coreutils-fix";
           patch = ./0001-fs-fcntl-accept-more-values-as-F_DUPFD_CLOEXEC-args.patch;
-        }
-        # speaker enablement; we assert on the relevant lsp-plugins patch
-        # before installing speakersafetyd to let the speakers work
-        { name = "speakers-1";
-          patch = fetchpatch {
-            url = "https://github.com/AsahiLinux/linux/commit/385ea7b5023486aba7919cec8b6b3f6a843a1013.patch";
-            hash = "sha256-u7IzhJbUgBPfhJXAcpHw1I6OPzPHc1UKYjH91Ep3QHQ=";
-          };
-        }
-        { name = "speakers-2";
-          patch = fetchpatch {
-            url = "https://github.com/AsahiLinux/linux/commit/6a24102c06c95951ab992e2d41336cc6d4bfdf23.patch";
-            hash = "sha256-wn5x2hN42/kCp/XHBvLWeNLfwlOBB+T6UeeMt2tSg3o=";
-          };
         }
       ] ++ _kernelPatches;
 
