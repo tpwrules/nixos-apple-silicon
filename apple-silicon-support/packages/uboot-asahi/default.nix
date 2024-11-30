0267e7aac5cdadd36345b509273fba3dadace44d
Thomas Watson <twatson52@icloud.com>
2024-11-30 16:06:09 -0600

    uboot-asahi: 2024.04-4-asahi -> 2024.10-1-asahi
    Keep bootmenu command enabled.

--- apple-silicon-support/packages/uboot-asahi/default.nix
+++ apple-silicon-support/packages/uboot-asahi/default.nix
@@ -9,10 +9,10 @@
     # tracking: https://pagure.io/fedora-asahi/uboot-tools/commits/main
     owner = "AsahiLinux";
     repo = "u-boot";
-    rev = "c134629a8bc448e979967bf0632fdd5bb42ee1d7";
-    hash = "sha256-EPdpyvblkvQrMCiPjX3Bnqp8s/3Yd/gYM1PQu4PDMhs=";
+    rev = "asahi-v2024.10-1";
+    hash = "sha256-gtXt+BglBdEKW7j3U2x2QeKGeDH1FdmAMPXk+ntkROo=";
   };
-  version = "2024.04-4-asahi";
+  version = "2024.10-1-asahi";
 
   defconfig = "apple_m1_defconfig";
   extraMeta.platforms = [ "aarch64-linux" ];
@@ -26,6 +26,7 @@
     CONFIG_VIDEO_FONT_8X16=n
     CONFIG_VIDEO_FONT_SUN12X22=n
     CONFIG_VIDEO_FONT_16X32=y
+    CONFIG_CMD_BOOTMENU=y
   '';
 }).overrideAttrs (o: {
   # nixos's downstream patches are not applicable
