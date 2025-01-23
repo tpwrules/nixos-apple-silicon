866c372382b02d7301910639e9750c3166db7da1
Thomas Watson <twatson52@icloud.com>
2025-01-22 23:17:22 -0600

    alsa-ucm-conf-aahi: rework for finalAttrs changes
    Don't override version attribute as that now gets picked up and used to
    download the wrong alsa-ucm-conf source code, breaking the build.
    
    Instead, directly generate a new and more meaningful name. Also don't
    inject the Asahi-specific attributes into the derivation to ensure they
    don't conflict.

--- apple-silicon-support/packages/alsa-ucm-conf-asahi/default.nix
+++ apple-silicon-support/packages/alsa-ucm-conf-asahi/default.nix
@@ -1,19 +1,22 @@
 { lib
 , fetchFromGitHub
-, alsa-ucm-conf }:
+, alsa-ucm-conf
+}:
 
-(alsa-ucm-conf.overrideAttrs (oldAttrs: rec {
-  version = "5";
+(alsa-ucm-conf.overrideAttrs (oldAttrs: let
+  versionAsahi = "5";
 
-  src_asahi = fetchFromGitHub {
+  srcAsahi = fetchFromGitHub {
     # tracking: https://src.fedoraproject.org/rpms/alsa-ucm-asahi
     owner = "AsahiLinux";
     repo = "alsa-ucm-conf-asahi";
-    rev = "v${version}";
+    rev = "v${versionAsahi}";
     hash = "sha256-daUNz5oUrPfSMO0Tqq/WbtiLHMOtPeQQlI+juGrhTxw=";
   };
-  
+in {
+  name = "${oldAttrs.pname}-${oldAttrs.version}-asahi-${versionAsahi}";
+
   postInstall = oldAttrs.postInstall or "" + ''
-    cp -r ${src_asahi}/ucm2 $out/share/alsa
+    cp -r ${srcAsahi}/ucm2 $out/share/alsa
   '';
 }))
