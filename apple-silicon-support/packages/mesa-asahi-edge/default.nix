5ef26258cbfdffd55b1a7544cb7704143895dabd
Oliver Bestmann <oliver.bestmann@googlemail.com>
2025-02-24 11:11:23 +0100

    mesa-asahi-edge: 20241211 -> 20250221

--- apple-silicon-support/packages/mesa-asahi-edge/default.nix
+++ apple-silicon-support/packages/mesa-asahi-edge/default.nix
@@ -7,25 +7,27 @@
   galliumDrivers = [ "softpipe" "llvmpipe" "asahi" ];
   vulkanDrivers = [ "swrast" "asahi" ];
 }).overrideAttrs (oldAttrs: {
-  version = "25.0.0-asahi";
+  version = "25.1.0-asahi";
   src = fetchFromGitLab {
     # tracking: https://pagure.io/fedora-asahi/mesa/commits/asahi
     domain = "gitlab.freedesktop.org";
     owner = "asahi";
     repo = "mesa";
-    rev = "asahi-20241211";
-    hash = "sha256-Ny4M/tkraVLhUK5y6Wt7md1QBtqQqPDUv+aY4MpNA6Y=";
+    rev = "asahi-20250221";
+    hash = "sha256-xt49IaylZYoH3LxYu6Uxd+qRrqQfjI6FDeAD8MLeWP8=";
   };
 
-  mesonFlags = let
-    badFlags = [
-      "-Dinstall-mesa-clc"
-      "-Dopencl-spirv"
-      "-Dgallium-nine"
-    ];
-    isBadFlagList = f: builtins.map (b: lib.hasPrefix b f) badFlags;
-    isGoodFlag = f: !(builtins.foldl' (x: y: x || y) false (isBadFlagList f));
-  in (builtins.filter isGoodFlag oldAttrs.mesonFlags) ++ [
+  mesonFlags =
+    let
+      badFlags = [
+        "-Dinstall-mesa-clc"
+        "-Dopencl-spirv"
+        "-Dgallium-nine"
+      ];
+      isBadFlagList = f: builtins.map (b: lib.hasPrefix b f) badFlags;
+      isGoodFlag = f: !(builtins.foldl' (x: y: x || y) false (isBadFlagList f));
+    in
+    (builtins.filter isGoodFlag oldAttrs.mesonFlags) ++ [
       # we do not build any graphics drivers these features can be enabled for
       "-Dgallium-va=disabled"
       "-Dgallium-vdpau=disabled"
@@ -40,5 +42,6 @@
   postInstall = (oldAttrs.postInstall or "") + ''
     # we don't build anything to go in this output but it needs to exist
     touch $spirv2dxil
+    touch $cross_tools
   '';
 })
